3b2e7ff3e8e8e150b9c8142e8fdc5b838836637d
Nelson Chen <crazysim@gmail.com>
2021-11-20 20:04:07 -0800

    Rough initial explanation

--- main.go
+++ main.go
@@ -1,32 +1,38 @@
 package main
 
+// Please see the README.md file for more information.
+
 import (
-	"io"
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		log.Printf("REQUEST | Escaped Path: %#v | Request Received: %#v", req.URL.EscapedPath(), req)
 
-	http.HandleFunc("/red%2Fblue.txt", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain")
-		w.WriteHeader(http.StatusOK)
-
-		_, err := io.WriteString(w, "Need %25 in URL!\n")
-		if err != nil {
-			log.Fatalf("Could not write: %s\n", err.Error())
-
+		if req.URL.EscapedPath() == "/red/blue.txt" {
+			w.WriteHeader(404)
+			_, err := fmt.Fprintf(w, "This path actually doesn't exist.")
+			if err != nil {
+				return
+			}
+			return
 		}
-	})
 
-	http.HandleFunc("/red/blue.txt", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain")
-		w.WriteHeader(http.StatusOK)
+		if req.URL.EscapedPath() == "/red%2Fblue.txt" {
+			w.WriteHeader(200)
+			_, err := fmt.Fprintf(w, "This path exists!")
+			if err != nil {
+				return
+			}
+			return
+		}
 
-		_, err := io.WriteString(w, "no!\n")
+		_, err := fmt.Fprintf(w, "Debug: Escaped Path Recieved\n%#v\n%#v", req.URL, req.URL.EscapedPath())
 		if err != nil {
-			log.Fatalf("Could not write: %s\n", err.Error())
-
+			return
 		}
 	})
 
